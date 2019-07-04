119ab03a22d7b4c5a310de2c6fbda2ae4ebadab3
unknown <pengqi@ESG.360ES.CN>
2019-07-04 17:15:37 +0800

    '修改invocationType'

--- simple-rpc-core/src/main/java/com/pq/rpc/common/enumeration/InvocationType.java
+++ simple-rpc-core/src/main/java/com/pq/rpc/common/enumeration/InvocationType.java
@@ -1,11 +1,14 @@
 package com.pq.rpc.common.enumeration;
 
 import com.pq.rpc.common.enumeration.support.ExtensionBaseType;
+import com.pq.rpc.config.ReferenceConfig;
 import com.pq.rpc.invocation.api.Invocation;
 import com.pq.rpc.invocation.async.AsyncInvocation;
 import com.pq.rpc.invocation.callback.CallbackInvocation;
 import com.pq.rpc.invocation.oneway.OneWayInvocation;
 import com.pq.rpc.invocation.sync.SyncInvocaton;
+import com.pq.rpc.protocol.api.InvokeParam;
+import com.pq.rpc.protocol.api.support.RPCInvokeParam;
 
 /**
  * 调用方式枚举类
@@ -14,7 +17,7 @@ import com.pq.rpc.invocation.sync.SyncInvocaton;
  * @author pengqi
  * create at 2019/7/3
  */
-public enum InvocationType implements ExtensionBaseType<Invocation> {
+public enum InvocationType {
     SYNC(new SyncInvocaton()),              //同步调用
     ASYNC(new AsyncInvocation()),           //异步调用
     CALLBACK(new CallbackInvocation()),     //回调方式
@@ -26,8 +29,16 @@ public enum InvocationType implements ExtensionBaseType<Invocation> {
         this.invocation = invocation;
     }
 
-    @Override
-    public Invocation getInstance() {
-        return invocation;
+    public Invocation get(InvokeParam invokeParam) {
+        ReferenceConfig referenceConfig = ((RPCInvokeParam)invokeParam).getReferenceConfig();
+        if(referenceConfig.isAsync()){
+            return ASYNC.invocation;
+        }else if(referenceConfig.isCallback()){
+            return CALLBACK.invocation;
+        }else if(referenceConfig.isOneWay()){
+            return ONEWAY.invocation;
+        }else{
+            return SYNC.invocation;
+        }
     }
 }
