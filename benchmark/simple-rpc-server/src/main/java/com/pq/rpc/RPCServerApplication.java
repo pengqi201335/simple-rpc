e89e001c1d6e1896cebe39bbbde871c3af4a1feb
pengqi <1157131987@qq.com>
2019-07-06 00:24:51 +0800

    添加测试

--- benchmark/simple-rpc-server/src/main/java/com/pq/rpc/RPCServerApplication.java
+++ benchmark/simple-rpc-server/src/main/java/com/pq/rpc/RPCServerApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
 public class RPCServerApplication implements CommandLineRunner {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws InterruptedException{
         SpringApplication app = new SpringApplication(RPCServerApplication.class);
-
+        app.run(args);
+        new CountDownLatch(1).await();
     }
 
     @Override
